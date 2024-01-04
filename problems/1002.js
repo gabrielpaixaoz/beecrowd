7cda78eca2e8b685d101abc93714a25d7541f3da
gabrielpaixaoz <gabrielpaix54@gmail.com>
2024-01-04 11:35:35 -0300

    d

---
+++ problems/1002.js
@@ -0,0 +1,14 @@
+export function problem (lines){
+    const A = lines[0]
+    const B = lines[1]
+
+    const X = Number(A) + Number(B)
+    
+    console.log(`X =${X}`)
+}
+
+
+/*
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n'); 
+*/
