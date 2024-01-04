dcbac452a0459254ef07e2fec4e104db1e4397be
gabrielpaixaoz <gabrielpaix54@gmail.com>
2024-01-04 11:30:23 -0300

    ok

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const getProblem = () => document.getElementById('problem')
+                                 .value = localStorage.getItem('problem') ?? 1000
+
+const setProblem = (problemNumber) => localStorage.setItem('problem', problemNumber)
+
+
+const solveProblem = async () =>  {
+    const input = document.getElementById('input')
+                          .value.split(/\r|\n/)
+    const problemNumber = document.getElementById('problem').value
+    setProblem (problemNumber)
+    
+    const {problem} = await import (`./problems/${problemNumber}.js`)
+    problem(input);
+
+}
+
+getProblem()
+document.getElementById('solve').addEventListener('click', solveProblem)
