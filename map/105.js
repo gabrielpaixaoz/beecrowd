7b911b45ba143ecc2d23c923d97bfdffb69c32db
gabrielpaixaoz <gabrielpaix54@gmail.com>
2024-02-01 06:26:15 -0300

    b

--- map/105.js
+++ map/105.js
@@ -1,3 +1,16 @@
-function calcularAreaTotal(){
+const dimensoes = [
+    { altura: 10, comprimento: 20},
+    { altura: 2, comprimento: 4},
+    { altura: 1, comprimento: 1},
+    { altura: 50, comprimento: 50}
+  ]
+    const soma = dimensoes.map(somando);
 
-}
+    function somando(item){
+        return item.altura * item.comprimento
+    }
+
+    let teste = soma.reduce((total,valor) => total + valor, 0);
+
+
+console.log(teste)
