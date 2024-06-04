ffc5bf03cea05bdc4232285c5e9d9f89df57bb96
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-06-04 14:52:25 -0300

    Create 1027.js

---
+++ problems/1027.js
@@ -0,0 +1,27 @@
+let posicoes_new = posicoes.map(posicao => parseFloat(posicao))
+
+    let posicaoX = posicoes_new[0]
+    let posicaoY = posicoes_new[1]
+    
+    
+    if(posicaoX > 0 && posicaoY > 0){
+        console.log("Q1")
+    }
+    else if(posicaoX > 0 && posicaoY < 0){
+        console.log("Q4")
+    }
+    else if(posicaoX < 0 && posicaoY > 0){
+        console.log("Q2")
+    }
+    else if(posicaoX < 0 && posicaoY < 0){
+        console.log("Q3")
+    }
+    else if(posicaoX == 0 && posicaoY > 0 || posicaoY < 0){
+        console.log("Eixo Y")
+    }
+    else if(posicaoX > 0 || posicaoX < 0 && posicaoY == 0){
+        console.log("Eixo X")
+    }
+    else if(posicaoX == 0 && posicaoY == 0){
+        console.log("Origem")
+    }
