8709d742e8d1367efe8367a47dea9e78a6875753
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-06-25 13:22:14 -0300

    daeaeda
    edaed

--- problems/1031.js
+++ problems/1031.js
@@ -1,11 +1,33 @@
 export function problem(lines){
-let [A, B, C] = lines[0].split(' ').map(Number)
-    
+let valores = lines[0].split(' ').map(Number)
+
+let ordernar = (numeros) =>[...numeros].sort((x,y) => y - x)
+
+let valoresOrdenados = ordernar(valores)
+
+let A = valoresOrdenados[0]
+let B = valoresOrdenados[1]
+let C = valoresOrdenados[2]
+
 if(A >= B + C){
     console.log("NAO FORMA TRIANGULO")
 }
-if((A**2) == (B**2) + (C**2)){
-    console.log("TRIANGULO RETANGULO")
+else{
+    if((A*A) == (B*B) + (C*C)){
+        console.log("TRIANGULO RETANGULO")
+    }
+    if((A*A) > (B*B) + (C*C)){
+        console.log("TRIANGULO OBTUSANGULO")
+    }
+    if((A*A) < (B*B) + (C*C)){
+        console.log("TRIANGULO ACUTANGULO")
+    }
+}
+if (A == B && A == C){
+    console.log("TRIANGULO EQUILATERO")
+}
+else if(A == B || A == C || C == B ){
+    console.log("TRIANGULO ISOSCELES")
 }
 
 }
