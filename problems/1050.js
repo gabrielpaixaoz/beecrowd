725810968ecf2600ea9c1555fc78d160e4bbd1e3
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-07-24 13:43:31 -0300

    daedaed

---
+++ problems/1050.js
@@ -0,0 +1,18 @@
+export function problem(lines){
+    const N = Number(lines[0])
+    let numeros_pares = []
+
+    if(5 < N < 2000){
+        for(let i = 1; i <= N; i++){
+            if(i % 2 == 0){
+                numeros_pares.push(i)
+            }
+        }
+
+        for(let i = 0; i <= numeros_pares.length; i++){
+            console.log(`${numeros_pares[i]}^2 = ${numeros_pares[i] * numeros_pares[i]}`)
+        }
+    }
+
+   
+}
