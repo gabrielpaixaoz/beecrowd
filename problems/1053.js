9515b744a083ab27c4274c382ad411c07885ca62
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-07-30 15:43:31 -0300

    Create 1053.js

---
+++ problems/1053.js
@@ -0,0 +1,14 @@
+export function problem(lines){
+    let qtd = lines[0]
+    let medias = []
+    let numeros = []
+    
+    for(let i = 1; i <= qtd; i++){
+        medias.push(lines[i])
+        
+    }
+
+    console.log(medias)
+
+
+}
