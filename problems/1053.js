bde5ef2d62e9a65edf65e4e25e0e81c29caba01b
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-07-31 14:07:59 -0300

    Update 1053.js

--- problems/1053.js
+++ problems/1053.js
@@ -5,17 +5,8 @@ export function problem(lines){
     for(let i = 1; i <= qtd; i++){
         medias = lines[i]
         let [A,B,C] = medias.split(' ').map(Number)
-        console.log(A)
-        console.log(B)
-        console.log(C)
-        console.log(medias)
-        let media = A *
+        
+        let media = (A * 2 + B * 3 + C * 5)/(2 + 3 + 5)
+        console.log(media.toFixed(1))
     }
 }
-
-/*
-3
-6.5 4.3 6.2
-5.1 4.2 8.1
-8.0 9.0 10.0
-*/
