06f64a750fe023d68c63178ab96b853b82bda297
gabrielpaixaoz <121585062+gabrielpaixaoz@users.noreply.github.com>
2024-07-31 14:04:33 -0300

    Update 1053.js

--- problems/1053.js
+++ problems/1053.js
@@ -1,23 +1,21 @@
 export function problem(lines){
     let qtd = lines[0]
-    let medias = []
-    let numeros = []
-    let resultados =[]
-    
+    let medias  = []
+
     for(let i = 1; i <= qtd; i++){
-        medias.push(lines[i])
-        
+        medias = lines[i]
+        let [A,B,C] = medias.split(' ').map(Number)
+        console.log(A)
+        console.log(B)
+        console.log(C)
+        console.log(medias)
+        let media = A *
     }
-
-    for(let i = 0; i <= medias.length; i++){
-        numeros = medias[i].split(' ').map(Number)
-       }
-       
-       
-       numeros = medias[0].split(' ').map(Number)
-
-    
-    console.log(numeros)
-
-
 }
+
+/*
+3
+6.5 4.3 6.2
+5.1 4.2 8.1
+8.0 9.0 10.0
+*/
